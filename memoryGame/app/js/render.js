2ea7d4cd0454d574346e198655cbc417088de2e7
glassysky <873661157@qq.com>
2015-11-17 23:05:37 +0800

    abolish table

--- memoryGame/app/js/render.js
+++ memoryGame/app/js/render.js
@@ -1,16 +1,7 @@
 define(['jquery'],function(){
 	return {
 		makeNumberTable : function(amount,time){
-			var headTpl = "";
-
-			for(var i = 0;i<21;i++){
-				if(i == 0){
-					headTpl = "<th>#</th>";
-				} else {
-					headTpl += "<th>" + i + "</th>";
-				}
-			}
-			$("#num-table").find("thead").find("tr").html(headTpl);
+			
 		}
 	};
 });
