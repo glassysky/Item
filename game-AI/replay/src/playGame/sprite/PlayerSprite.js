a11e67c2a24a4c4700def3826a061910a1aff3a4
glassysky <873661157@qq.com>
2015-11-25 01:58:43 +0800

    everyday final. fightinggit add -A!

--- game-AI/replay/src/playGame/sprite/PlayerSprite.js
+++ game-AI/replay/src/playGame/sprite/PlayerSprite.js
@@ -1,3 +1,19 @@
 /**
  * Created by cui on 2015/11/24.
  */
+var PlayerSprite = cc.Sprite.extend({
+    _x : null,
+    _y : null,
+    _sptPlayer : null,
+
+    ctor : function(aTexture){
+        this._super(aTexture);
+
+        this._sptPlayer = new cc.Sprite(res.player);
+        this.addChild(this._sptPlayer);
+    },
+
+    PlayerSpriteMove : function(){
+
+    }
+})
