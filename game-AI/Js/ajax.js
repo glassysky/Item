2e47b66b0147fd82f7272f566686879eff47ba9b
glassysky <873661157@qq.com>
2015-11-30 04:04:51 +0800

    gona die fucked

--- game-AI/Js/ajax.js
+++ game-AI/Js/ajax.js
@@ -11,7 +11,7 @@ define(['jquery'],function(){
 
                 $.ajax({
                     type : 'post',
-                    url : '/playai/test/enemy.json',
+                    url : 'http://localhost/playai/test/enemy.json',
                     success : function(callback){
                         for(var i in callback){
                             if(callback.hasOwnProperty(i)){
@@ -41,22 +41,34 @@ define(['jquery'],function(){
 
                 $.ajax({
                     type : 'post',
-                    url : '/playai/test/battle.json',
+                    url : 'http://localhost/playai/test/battle.json',
                     success : function(callback){
-                        for(var i in callback){
-                            if(callback.hasOwnProperty(i)){
-                                length++;
-                            }
-                        }
+                        var msg = callback["msg"];
 
-                        for(var i = 1;i<length+1;i++){
+                        console.log(msg);
+
+                        for(var i = 0;i<msg.length;i++){
+                            switch (msg[i].result){
+                                case "1":
+                                    msg[i].result = msg[i].uid + "胜";
+                                    break;
+                                case "0.5":
+                                    msg[i].result = msg[i].oid + "胜";
+                                    break;
+                                case "0":
+                                    msg[i].result = "平局";
+                                    break;
+                                default:
+                                    break;
+                            }
+
                             tpl = "<tr>"+
-                                "<td>" + i + "</td>" +
-                                "<td>" + callback[i].attacker + "</td>" +
-                                "<td>" + callback[i].defender + "</td>" +
-                                "<td>" + callback[i].result +"胜" + "</td>" +
-                                "<td>" + callback[i].date + "</td>" +
-                                "<td><a class='btn btn-primary battle-replay' id='" + i + "detail'>对战细节</a></td>" +
+                                "<td>" + (i+1) + "</td>" +
+                                "<td>" + msg[i].uid + "</td>" +
+                                "<td>" + msg[i].oid + "</td>" +
+                                "<td>" + msg[i].result + "</td>" +
+                                "<td>" + msg[i].begintime + "</td>" +
+                                "<td><a href='" + msg[i].process + "' class='btn btn-primary battle-replay' id='" + msg[i].id + "detail'>对战细节</a></td>" +
                                 "</tr>";
                             str = str + tpl;
                         }
@@ -76,21 +88,16 @@ define(['jquery'],function(){
                     length = 0;
                 $.ajax({
                     type : 'post',
-                    url : '/playai/test/rank.json',
+                    url : 'http://localhost/playai/test/rank.json',
                     success : function(callback){
+                        var msg = callback["msg"];
 
-                        for(var i in callback){
-                            if(callback.hasOwnProperty(i)){
-                                length ++;
-                            }
-                        }
-
-                        for(var i = 0;i<length;i++){
+                        for(var i = 0;i<msg.length;i++){
                             tpl = "<tr>" +
                                 "<td>" + (i+1) + "</td>" +
-                                "<td>" + callback[i+1].nickname + "</td>" +
-                                "<td>" + callback[i+1].score + "</td>" +
-                                "<td><a href='' class='btn btn-primary' id='" + (i+1) + "PK'>PK一下</a></td>" +
+                                "<td>" + msg[i].nickname + "</td>" +
+                                "<td>" + msg[i].score + "</td>" +
+                                "<td><a href='' class='btn btn-primary' id='" + msg[i].id + "PK'>PK一下</a></td>" +
                                 "</tr>";
                             str = str + tpl;
                         }
@@ -101,6 +108,62 @@ define(['jquery'],function(){
                         console.log("error");
                     }
                 })
+            });
+        },
+        codeVersion : function(){
+            var str = "",
+                tpl = "",
+                length = 0;
+
+            $.ajax({
+                type : 'post',
+                url : 'http://localhost/playai/test/codeversion.json',
+                success : function(callback){
+                    var msg = callback["msg"];
+
+                    console.log(msg);
+
+                    for(var i = 0;i<msg.length;i++){
+                        tpl = "<div class='ver-item'>" +
+                        "<div class='ver-pic'>" +
+                            "<p>" + msg[i].filename + "</p>" +
+                            "</div>" +
+                            "<div class='ver-detail'>" +
+                            "<span>版本详细信息：</span>" +
+                        "<span>编程语言：" + msg[i].filetype + "</span>" +
+                        "<span>文件大小：" + msg[i].filesize + "</span>" +
+                        //"<span>提交日期：2015/10/23</span>" +
+                        //"<span>对战次数：20</span>" +
+                        //"<span>获胜次数：10</span>" +
+                        //"<span>Commit: Init. Commit.</span>" +
+                        "</div>" +
+                        "</div>";
+
+                        str = str + tpl;
+                    }
+
+                    $("#verlist").append(str);
+                    $("#currentVersion").html("Ver " + msg.length + ".0");
+                }
+            });
+        },
+        uploadCode : function(){
+
+            $("#submitCode").on("click",function(){
+                var file = $("#file-select").val(),
+                    filetype = $("#filetype-select").val();
+                $.ajax({
+                    type : 'post',
+                    url : 'http://localhost/playai/game.php/User/Upload/userUpload',
+                    dataType : 'json',
+                    data : {
+                        "file" : file,
+                        "filetype" : filetype
+                    },
+                    success : function(callback){
+
+                    }
+                });
             });
         }
     }
--- game-AI/Js/codeupdate.js
+++ game-AI/Js/codeupdate.js
@@ -1,19 +1,7 @@
 /**
  * Created by cui on 2015/11/29.
  */
-define(['jquery'],function(){
-    $("#submitCode").on("click",function(){
-        var codeLang = $("select").val(),
-            file = $("input").val();
+define(['jquery','ajax'],function($,ajax){
+    ajax.codeVersion();
 
-        $.ajax({
-            type : "post",
-            url : "",
-            dataType : 'json',
-            data : {
-                "codeLang" : codeLang,
-                "file" : file
-            }
-        });
-    });
 });
